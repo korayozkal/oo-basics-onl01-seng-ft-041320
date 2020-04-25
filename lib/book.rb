8ad2f8db850906c7ec94631121c5eb17040e6885
Koray Ozkal <koryozkal@gmail.com>
2020-04-25 04:02:10 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,10 @@
 # Don't forget to run tests with 
 # learn spec/01_book_spec.rb
+# title, author, page_count, genre
 
 class Book
 
-def initialize(title, author, page_count, genre)
+def initialize(title)
 end
 
 
